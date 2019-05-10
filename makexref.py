b091340e4ce5985903c0448ef044074b81bc064e
David Singer <david@dsinger.com>
2019-05-10 16:50:57 -0700

    Multiple fixes and improvements, see details.
    Move project descriptions into 'projects' subdirectory.
    Use Google Spreadsheet (whose name is hardcoded, sorry) instead of
       Excel Spreadsheet for path/project xref
    Fix "Develop a Communications Plan" project description

--- makexref.py
+++ makexref.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 from __future__ import print_function
-import xlrd
+from googleapiclient import discovery
 import os,sys
+from credentials import apikey
 
 class Path:
     paths = {}
@@ -18,28 +19,15 @@ class Path:
         self.blurb = ''
 
 class Project:
-    def __init__(self, name, order, path, value):
+    def __init__(self, name, path, value):
         self.name = name
-        self.order = order
         self.value = value
         self.level = int(self.value[0])
         self.required = 'R' in self.value
-        self.key = (self.level, not self.required, self.value, self.order)
+        self.isIceBreaker = self.name == 'Ice Breaker'
+        self.key = (self.level, not self.isIceBreaker, not self.required, self.value, self.name)
         Path.get(path).projects.append(self)
-        
-class Multout:
-    def __init__(self):
-        self.files = []
-    def add(self, handle):
-        self.files.append(handle)
-    def write(self, str):
-        for f in self.files:
-            f.write(str)
-    def delete(self, handle):
-        try:
-            del self.files[self.files.index(handle)]
-        except Exception:
-            print(sys.exc_info())
+    
         
 levels = {1: "Mastering Fundamentals",
           2: "Learning Your Style",
@@ -61,43 +49,41 @@ def cleanup(s):
     """Remove extraneous newlines (all not before a tag)"""
     return s.replace('\n<', '\x00').replace('\n',' ').replace('\x00','\n<').strip()
 
-book = xlrd.open_workbook('Projects.xlsx')
-os.chdir('data')
+# Connect to the Google Spreadsheet with path and project info
+service = discovery.build('sheets','v4',developerKey=apikey)
+sheetid = '199fHCu4LJ9UvJJCCiNpytvoHn0kxQJXSm93Z_-EFPW0'
+request = service.spreadsheets().values()
 
 # Get the projects
-sheet = book.sheet_by_name('Projects')
-colnames = sheet.row_values(0)
+result = request.get(spreadsheetId=sheetid, range='Projects').execute().get('values',[])
+colnames = result[0]   
 
 # Create paths
 namecol = colnames.index('Name')
-ordercol = colnames.index('Order')
-pathcols = range(namecol+1,sheet.ncols)
+pathcols = range(namecol+1,len(colnames))
 
 
 # Create projects and assign to paths
-for rownum in range(1,sheet.nrows):
-    order = sheet.cell_value(rownum, ordercol)
-    if not order:
-        order = 0
-    name = sheet.cell_value(rownum, namecol)
+for row in result[1:]:
+    name = row[namecol]
     for p in pathcols:
-        val = ('%s' % sheet.cell_value(rownum, p)).strip()
+        try:
+            val = ('%s' % row[p]).strip()
+        except IndexError:
+            val = ''
         if val:
-            Project(name, order, colnames[p], val)
+            Project(name, colnames[p], val)
             
 # Now, get the blurbs for each path
-sheet = book.sheet_by_name('Paths')
-for rownum in range(1, sheet.nrows):
-    pathname = sheet.cell_value(rownum, 0)
+result = request.get(spreadsheetId=sheetid, range='Paths').execute().get('values',[])
+for row in result[1:]:
+    pathname = row[0]
     path = Path.get(pathname)
-    path.blurb = sheet.cell_value(rownum, 1)
+    path.blurb = row[1]
             
 # OK, now create the output
-            
-outfiles = Multout()
-allout = open("Allpaths.html", "w", encoding="utf-8")
-outfiles.add(allout)
-allout.write(styleinfo)
+outfile = open(os.path.join("output", "allpaths.html"), "w", encoding="utf-8")
+outfile.write(styleinfo)
 
 
 for p in pathcols:
@@ -105,58 +91,50 @@ for p in pathcols:
     pathid = ''.join([part[0:4] for part in pathname.lower().split()[0:2]])
     path = Path.get(pathname)
     path.projects.sort(key=lambda item:item.key)
-    with open('Path ' + pathname + '.html', 'w', encoding='utf-8') as outfile:
-        outfiles.add(outfile)
-        outfile.write(styleinfo)
-        outfiles.write('<div class="pathname-header">\n')
-        # Write the expandable header for the all-in-one file
-        allout.write('<div class="pathname" onclick="jQuery(\'#%sopen, #%sclosed, #%sdesc\').toggle()">' % (pathid, pathid, pathid))
-        allout.write('<span id="%sopen" style="display:none">&#x2296;</span><span id="%sclosed">&#x2295;</span> %s\n' % (pathid, pathid, pathname)) 
-        allout.write('</div>\n')
-        
-        # Write the normal header for the file-per-path version
-        outfile.write('<h2 class="pathname">%s</h2>\n' % pathname)
-        outfiles.write('</div>')  # Close the pathname header
-        outfiles.write('<p class="blurb">%s</p>\n' % path.blurb)
-        
-        # Write the wrapper for the all-in-one file
-        allout.write('<div id="%sdesc" style="display:none">\n' % pathid)
-        
-        # Write the details
-        level = 0
-        inelectives = False
-        for item in path.projects:
-            if item.level != level:
-                if inelectives:
-                  outfiles.write('</div>\n')
-                outfiles.write('<h3 class="level">Level %s: %s</h3>\n' % (item.level, levels[item.level]))
-                level = item.level
-                inelectives = False
-                itemnum = 0
-            itemnum += 1
-            itemid = '%s%s%s' % (pathid, level, itemnum)
-            if not inelectives and not item.required:
-                outfiles.write('<div class="electives-header">Electives (Choose %d)</div>\n' % [0, 0, 0, 2, 1, 1][level])   
-                outfiles.write('<div class="electives">\n')
-                inelectives = True
-            outfiles.write('<div class="%s-project">\n' % ('req' if item.required else 'elective'))
-
-            outfiles.write('<div class="projname" onclick="jQuery(\'#%sopen, #%sclosed, #%sdesc\').toggle()">' % (itemid, itemid, itemid))
-            outfiles.write('<span id="%sopen" style="display:none">&#x2296;</span><span id="%sclosed">&#x2295;</span> %s\n' % (itemid, itemid, item.name)) 
-            outfiles.write('</div>\n')
-            outfiles.write('<div id="%sdesc" class="projdesc" style="display:none;">\n' % itemid)
-            outfiles.write(cleanup(open(item.name+'.html', 'r', encoding='utf-8').read().encode('ascii','xmlcharrefreplace').decode()))
-            outfiles.write('</div>\n')
-            outfiles.write('</div>\n')
-        if inelectives:
-            outfiles.write('</div>\n')
-        
-        outfiles.delete(outfile)
-        
-        # Close the wrapper
-        allout.write('</div>\n')  
-
+    outfile.write('<div class="pathname-header">\n')
+    # Write the expandable header 
+    outfile.write('<div class="pathname" onclick="jQuery(\'#%sopen, #%sclosed, #%sdesc\').toggle()">' % (pathid, pathid, pathid))
+    outfile.write('<span id="%sopen" style="display:none">&#x2296;</span><span id="%sclosed">&#x2295;</span> %s\n' % (pathid, pathid, pathname)) 
+    outfile.write('</div>\n')
+    outfile.write('</div>')  # Close the pathname header
+    outfile.write('<p class="blurb">%s</p>\n' % path.blurb)
     
+    # Write the wrapper 
+    outfile.write('<div id="%sdesc" style="display:none">\n' % pathid)
+    
+    # Write the details
+    level = 0
+    inelectives = False
+    for item in path.projects:
+        if item.level != level:
+            if inelectives:
+                outfile.write('</div>\n')
+            outfile.write('<h3 class="level">Level %s: %s</h3>\n' % (item.level, levels[item.level]))
+            level = item.level
+            inelectives = False
+            itemnum = 0
+        itemnum += 1
+        itemid = '%s%s%s' % (pathid, level, itemnum)
+        if not inelectives and not item.required:
+            outfile.write('<div class="electives-header">Electives (Choose %d)</div>\n' % [0, 0, 0, 2, 1, 1][level])   
+            outfile.write('<div class="electives">\n')
+            inelectives = True
+        outfile.write('<div class="%s-project">\n' % ('req' if item.required else 'elective'))
+
+        outfile.write('<div class="projname" onclick="jQuery(\'#%sopen, #%sclosed, #%sdesc\').toggle()">' % (itemid, itemid, itemid))
+        outfile.write('<span id="%sopen" style="display:none">&#x2296;</span><span id="%sclosed">&#x2295;</span> %s\n' % (itemid, itemid, item.name)) 
+        outfile.write('</div>\n')
+        outfile.write('<div id="%sdesc" class="projdesc" style="display:none;">\n' % itemid)
+        outfile.write(cleanup(open('projects/'+item.name+'.html', 'r', encoding='utf-8').read().encode('ascii','xmlcharrefreplace').decode()))
+        outfile.write('</div>\n')
+        outfile.write('</div>\n')
+    if inelectives:
+        outfile.write('</div>\n')
+    
+    # Close the wrapper
+    outfile.write('</div>\n')  
+
+
             
 
     
